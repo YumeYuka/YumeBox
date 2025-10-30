70fe8c5d2d05cf5fe4b93cafda900c316c503d8b
梦璃酱 <Miao@YumeYuka.plus>
2025-10-30 21:50:45 +0800

    chore(release): bump version to 2.11.18
    Signed-off-by: 梦璃酱 <Miao@YumeYuka.plus>

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,8 @@ subprojects {
             minSdk = 26
             targetSdk = 36
 
-            versionName = "2.11.17"
-            versionCode = 211017
+            versionName = "2.11.18"
+            versionCode = 211018
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
